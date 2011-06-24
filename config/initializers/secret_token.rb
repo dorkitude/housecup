6c045020c246f9d6cc94494a15e608abdf53d890
dorkitude <kyle@kylewild.com>
2011-06-24 14:47:36 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Housecup::Application.config.secret_token = 'e5d3b7cb613e4c5fc50980549e4969a0a43eb98997a3c851a0539d19e74bb521ba5ec87f5db6cfcddc09b15669f63a16cbc47a449e62d51b9a53e456f83c815b'
